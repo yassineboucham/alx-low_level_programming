648f795ecdc4a166fb0e291281ee58a22d66623e
root <yassineboucham@.com>
2023-09-08 11:17:13 -0700

    yassineboucham

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
 /**
- * main - is a compail fuction
- * Return: is return a 0 
+ * main - is a function main
+ * Return: is return 0
 */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  retunr (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
