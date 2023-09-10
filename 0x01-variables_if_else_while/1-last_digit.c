c427c6e5ffe03e5540761b35bf45497f173ef4d2
root <yassineboucham@.com>
2023-09-10 14:33:13 -0700

    yassineboucham

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (digit > 5)
-printf("Last digit of %d is %d and is greater than 5", n, digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 else if (digit == 0)
-printf("Last digit of %d is %d and is 0", n , digit);
+printf("Last digit of %d is %d and is 0\n", n , digit);
 else if (digit != 0 && digit < 6)
-printf("Last digit of %d is %d and is less than 6 and not 0", n, digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 return (0);
 }
