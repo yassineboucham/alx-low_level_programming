f1dda46919a70952cc54acd743fc7fc5c9245a5f
root <yassineboucham@.com>
2023-09-11 09:39:28 -0700

    yassineboucham

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,12 @@
  * main - the main fuction
  * Return: the return value is 0
 */
-int main()
+int main(void)
 {
 char c1 = 'a', c2 = 'A';
-for (c1 = 'a'; c1 <= 'z'; c1++)
+while (c1 <= 'z')
 putchar(c1);
-for (c2 = 'A'; c2 <= 'Z'; c2++)
+while (c2 <= 'Z')
 putchar(c2);
 putchar('\n');
 }
