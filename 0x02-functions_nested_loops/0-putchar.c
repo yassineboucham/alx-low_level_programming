dfe9bda35003cdaa29ae3aa2adb7b2ce86ac37db
root <yassineboucham@.com>
2023-09-13 16:18:58 -0700

    yassineboucham

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - the main function
  * Return: the retur value is 0
 */
 int main(void)
 {
-printf("_putchar");
+int i = 0;
+char ch[] = "_putchar";
+while (ch[i] != '\0')
+{
+_putchar(ch[i]);
+i++;
+}
 return (0);
 }
