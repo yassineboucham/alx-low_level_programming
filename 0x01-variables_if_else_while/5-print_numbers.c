0525626648c41e7a156e087967b19955c57f57fd
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-14 12:39:39 +0100

    mm

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: returnung 0
+*/
+int main(void)
+{
+for (char c = '0'; c < '10'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
