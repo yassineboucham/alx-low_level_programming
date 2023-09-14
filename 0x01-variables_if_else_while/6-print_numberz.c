0530939d7904b95f5c4c8948d2fb4cd05f9a30d0
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-14 13:08:30 +0100

    6-print-nubers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - the main function
+* Return: returnung 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+	}
+	putchar('\n');
+	return (0);
+}
