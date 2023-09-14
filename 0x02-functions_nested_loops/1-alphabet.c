36c9be3ef310683c89d35d57204625f5753a0e53
root <yassineboucham@.com>
2023-09-13 17:32:49 -0700

    yassineboucham

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - the function for printing the alphabet
+ * @c: the variable for loping the alphabet
+*/
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
