6a90ccaee19cee987845b05e1dbf5d0e1de5cb4d
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-15 17:34:33 +0100

    all

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - the function that multipay tow nubers
+ * @a: the variable nuber 1
+ * @b: th variabel number 2
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - function that prints the numbers, from 0 to 9
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
