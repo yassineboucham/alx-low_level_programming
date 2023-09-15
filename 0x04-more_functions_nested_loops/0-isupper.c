5ebfaa53402388f6681b15bdb5516e77c9caa39c
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-15 01:58:12 +0100

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - the function for chek the upper
+* @c: is a var
+* Return: the returning value is 1 if it c upper else 0
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
