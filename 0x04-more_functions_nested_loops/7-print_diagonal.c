4520d6801d197f807e79a5aa8dae2c87f076c99c
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-17 18:07:56 +0100

    ex 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,17 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-if (n > 0)
+if (n <= 0)
+_putchar('\n');
+else
 {
-	for (i = 1; i <= n; i++)
+    	for (i = 1; i <= n; i++)
 	{
-        for (j = 0; j < i; j++)
+        for (j = 1; j <= i; j++)
             _putchar(' '); 
         _putchar('\\');
   	    _putchar('\n');
 	}
 }
-else
-	_putchar('\n');
+	
 }
