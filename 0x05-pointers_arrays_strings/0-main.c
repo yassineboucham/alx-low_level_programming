8c88133c5eb2b3a8b6c7ab1c8120d7905b07bbd7
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-19 00:34:26 +0100

    ex0

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,5 +1,12 @@
-#include "main.h"
 #include <stdio.h>
+/**
+ * reset_to_98 - function that updates the value it points to 98.
+ * @n: the variable changing
+*/
+void reset_to_98(int *n)
+{
+	*n = 89;
+}
 
 /**
 * main - check the code
