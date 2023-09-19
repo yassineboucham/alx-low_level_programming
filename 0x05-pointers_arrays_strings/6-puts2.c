683d4165d2a4a6f1bb6f3c4b73aa598ed0d8f888
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-19 03:53:35 +0100

    ex6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,12 @@
 */
 void puts2(char *str)
 {
-
-while (*str != '\0')
+int i;
+while (str[i] != '\0')
 {
 	if (*str % 2 == 0)
-	_putchar(*str);
-	str++;
+	_putchar(str[i]);
+	i++;
 }
 _putchar('\n');
 }
