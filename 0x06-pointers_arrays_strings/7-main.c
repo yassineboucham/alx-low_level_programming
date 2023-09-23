6c8e740e743f715977c7b334272a67accfdbd40e
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-23 07:25:47 +0100

    ex7

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+char *leet(char *);
+
 /**
-* main - check the code for
+* main - check the code for Holberton School students.
 *
 * Return: Always 0.
 */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char s[] = "Talk is cheap. Show me the code.\n";
 
 	char *p;
 
