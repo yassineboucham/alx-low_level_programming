7496aa6ccf717405ce5e5d12c2642f52a905b0c3
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-23 01:29:32 +0100

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *leet -  a function that encodes a string into 1337.
+ * @str: the pointer of a string
+ * Return: returning str
+*/
+char *leet(char *str)
+{
+	char *ptr = str;
+
+	char let[] = {'a', 'e', 'o', 't', 'l'};
+
+	char nb[] = {'4', '3', '0', '7', '1'};
+
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i < 4; i++)
+		{
+			if (*str == let[i] || *str == let[i] - 32)
+			*str = nb[i];
+		}
+		str++;
+	}
+	return (ptr);
+}
