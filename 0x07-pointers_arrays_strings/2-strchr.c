f5a04c2309d1b5a07ee35ec848c4defc7ffd876d
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-26 00:41:49 +0100

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_strchr - a function that locates a character in a string.
+* @s: the poienter of a string
+* @c: the character of astring
+* Return: the return value is s
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+			s++;
+	}
+	return ('\0');
+}
