c3614e6aff17dba465a4c34e931d1d906d1012e9
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-26 00:50:39 +0100

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,15 @@
 */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
+	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			return (s[i]);
+			return (s + i);
 		}
+		i++;
 	}
 	return ('\0');
 }
