3075a1c42cc22cff0561323205d92c40cd110a6d
root <root@DESKTOP-GSM3MPE.localdomain>
2023-09-29 00:29:47 +0100

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - a function that prints a string, followed by a new line
- * @s: the variable s
+* _puts_recursion - a function that prints a string, followed by a new line
+* @s: the variable s
 */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s == '\0')
-	break;
-_puts_recursion(s);
+	_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
 }
