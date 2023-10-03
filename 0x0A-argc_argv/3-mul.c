cef292bd9addbb833aadd94b674983a712757ebc
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-04 00:21:32 +0100

    5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,9 +10,12 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	if (argc <= 3)
+	if (argc == 3)
 		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	else
+	{
 		printf("Error");
-return (0);
+		return (1);
+	}
+
 }
