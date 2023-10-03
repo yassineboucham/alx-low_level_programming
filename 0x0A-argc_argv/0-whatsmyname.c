dff5a92281879115d581d24fced90285dd0ebdbf
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-03 23:00:55 +0100

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - the main function
+* @argc: int
+* @argv: string
+* Return: 0;
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
