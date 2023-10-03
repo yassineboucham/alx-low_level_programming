0685be3e4209f8fdcb5aa829a61fe45c2ba97957
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-03 02:02:06 +0100

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,32 +6,17 @@
 */
 int _atoi(char *s)
 {
-
-	int result = 0;
+	int n = 0;
 
 	int sign = 1;
 
-	while (*s == ' ' || *s == '\t' || *s == '\n')
-		s++;
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-		s++;
-	while (*s >= '0' && *s <= '9')
-	{
-		if (result > (2147483647 - (*s - '0')) / 10)
-			{
-			if (sign == 1)
-				return (2147483647);
-			else
-				return (-2147483648);
-			}
-		result = result * 10 + (*s - '0');
-		s++;
-	}
-
-	return (result * sign);
+	do {
+		if (*s == '-')
+		sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		n = (n * 10) + (*s - '0');
+		else if (n > 0)
+		break;
+	} while (*s++);
+	return (num * sign);
 }
