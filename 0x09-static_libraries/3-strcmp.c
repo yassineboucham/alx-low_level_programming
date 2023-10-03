3b9fc8d1464757e668ecc1b285ac166cb4429fc7
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-03 11:02:46 +0100

    yarbi tkhdem

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -7,17 +7,5 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
-
-	while (*s1)
-	{
-		if (*s1 != *s2)
-		{
-			res += (int)*s1 - (int)*s2;
-			break;
-		}
-		s1++;
-		s2++;
-	}
-	return (res);
+	return (0);
 }
