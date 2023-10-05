f08fbaf54748832f72c0185084caaeaa2b784ed2
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-05 02:14:22 +0100

    ali

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,20 +23,15 @@ char *str_concat(char *s1, char *s2)
 {
 int j = 0, i = 0;
 char *ptr;
-if (!s1)
-return (s2);
-if (!s2)
-return (s1);
-
 ptr = (char *)malloc((strlent(s1) + (strlent(s2) + 1)) * sizeof(char));
 if (ptr == NULL)
 return (NULL);
-while (s1[j])
+while (s1[j] && s1)
 {
 ptr[j] = s1[j];
 j++;
 }
-while (s2[i])
+while (s2[i] && s2)
 ptr[j++] = s2[i++];
 ptr[j++] = '\0';
 return (ptr);
