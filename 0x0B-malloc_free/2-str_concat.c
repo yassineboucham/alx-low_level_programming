ee68b4e14d40bdb644687e94b3cb3fb70d755ff2
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-06 00:54:16 +0100

    hhh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,24 +21,24 @@ int strlent(char *str)
 */
 char *str_concat(char *s1, char *s2)
 {
-int i = 0;
+int i;
 
 char *ptr;
 int size1 = strlent(s1);
 int size2 = strlent(s2);
 if (s1 == NULL)
-s1 = '\0';
+s1 = "\0";
 if (s2 == NULL)
-s2 = '\0';
+s2 = "\0";
 ptr = malloc((size1 + size2) * sizeof(char) + 1);
 if (ptr == 0)
 return (0);
-for (i = 0; i < (size1 + size2); i++)
+for (i = 0; i <= (size1 + size2); i++)
 {
 	if (i < size1)
 		ptr[i] = s1[i];
 	else
-		ptr[i] = ptr[i - size1];
+		ptr[i] = s2[i - size1];
 }
 ptr[i] = '\0';
 return (ptr);
