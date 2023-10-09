a1d72b6586f9998f78a917f6ba715b0abd7994b7
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-09 02:28:52 +0100

    5

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,15 +8,15 @@
 */
 int *array_range(int min, int max)
 {
-	int *ptr, i, j, s;
+	int *ptr, i, j = 0, s;
 
 	if (min > max)
 	return (NULL);
-	s = max - min;
+	s = (max - min) + 1;
 	ptr = malloc(sizeof(int) * s);
 	if (ptr == NULL)
 	return (NULL);
-	for (i = min; i < max; i++)
+	for (i = min; i <= max; i++)
 	{
 		ptr[j] = i;
 		j++;
