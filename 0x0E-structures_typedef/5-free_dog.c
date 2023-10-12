4cda7770f3fbfc1c675afc83bd70a104fd84dc89
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-12 03:47:15 +0100

    all

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_dog -  function that frees dogs.
- * @d: pointer
+* free_dog -  function that frees dogs.
+* @d: pointer
 */
 void free_dog(dog_t *d)
 {
-	free(d);
+	free((*d).name);
+	free((*d).owner);
+	free(*d);
 }
