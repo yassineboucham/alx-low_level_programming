12ad41b05f079b1506ccb2bc89a54a999c767a48
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-12 03:23:05 +0100

    all

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,12 +9,12 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 	if ((*d).name == NULL)
-	printf("(nil)");
+	printf("Name: (nil)\n");
 	else
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	if ((*d).owner == NULL)
-	printf("(nil)");
+	printf("Owner: (nil)\n");
 	else
 	printf("Owner: %s\n", (*d).owner);
 	}
