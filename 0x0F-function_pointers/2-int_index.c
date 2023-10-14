3ff41da5c8bf01349156991ec917ddaf813506f5
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-14 01:25:03 +0100

    all

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,21 @@
 #include "function_pointers.h"
+/**
+* int_index - function that searches for an integer.
+* @array: int
+* @size: int
+* @cmp: int
+* Return: int
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	cmp(array[size]);
+	int i;
+
+	if (!array || !size || !cmp || size < 0)
+	return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+	return (-1);
 }
