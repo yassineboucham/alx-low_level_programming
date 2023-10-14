843964f0c0b95ca3f52cbadeffb0e5dda9d9f6ee
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-14 01:01:52 +0100

    all&

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include "main.h"
-
+/**
+* print_name - function that prints a name.
+* @name: string
+* @f: function
+*/
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 	f(name);
 }
