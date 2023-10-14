2d74e5f9dc5e125150d2fc198b1e3da2b1b9aad7
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-14 23:56:25 +0100

    h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+/**
+* struct token - token function
+* @token: char
+* @f: funtion to pointer
+*/
+typedef struct token
+{
+	char *token;
+
+	void (*f)(char *, var_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
+
 #endif
---
+++ remove_ex.sh
@@ -0,0 +1,2 @@
+find . -maxdepth 1 -type f -executable -exec rm -i {} \;
+rm *-main.c
