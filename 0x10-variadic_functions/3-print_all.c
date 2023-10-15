c128409ff03f5c2de1430c54e326eb266b99d22b
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-15 02:11:33 +0100

    alx

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,39 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 void print_all(const char * const format, ...)
 {
-    
+	int i;
+
+va_list op;
+va_start(op, format);
+if (format)
+{
+while (format[i] != '\0')
+{
+	switch (format[i])
+	{
+	case 'c':
+		printf("%c", va_arg(op, int));
+		break;
+	case 'i':
+		printf("%d", va_arg(op, int));
+		break;
+	case 'f':
+		printf("%f", va_arg(op, double));
+		break;
+	case 's':
+		printf("%s", va_arg(op, char *));
+		break;
+	default:
+	    i++;
+	continue;
+}
+	if (format[i + 1] != '\0')
+	printf(", ");
+	i++;
+}
+}
+printf("\n");
+va_end(op);
 }
