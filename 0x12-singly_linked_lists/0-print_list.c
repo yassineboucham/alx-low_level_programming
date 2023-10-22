c6f0f85d6acc0c162574dc050851688e60d65946
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-23 00:55:38 +0100

    0 and 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - Write a function that prints all the elements of a list_t list.
+ * @h: list_t
+ * Return: i
+*/
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * list_len - function that returns the number of elements.
+ * @h: list_t
+ * Return: i
+*/
 size_t list_len(const list_t *h)
 {
 	const list_t *corent = h;
