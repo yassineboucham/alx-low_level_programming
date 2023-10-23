da34b921c94f659e252527632fda38c3fa5003a1
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-23 02:12:56 +0100

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,27 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * *add_node -  function that adds a new node
+ * @head: list_t
+ * @str: string
+ * Return: curr
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t curr;
+	list_t *curr;
 
-	curr.str = strdup(str);
-	curr.len = strlen(str);
-	curr.next = **head;
-	head = &curr;
-	return (head);
+	curr = (list_t *)malloc(sizeof(list_t));
+	if (curr == NULL)
+		return (NULL);
+	curr->str = strdup(str);
+	if (curr->str == NULL)
+	{
+		free(curr);
+		return (NULL);
+	}
+	curr->len = strlen(str);
+	curr->next = *head;
+	*head = curr;
+	return (curr);
 }
