a99b159c83ada4d67de0ef8c6263ccda7e358045
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-24 23:40:37 +0100

    pop 6

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	listint_t *tmp = head;
+
+	while (i++ < index)
+	{
+		tmp = tmp->next;
+	}
+	return (tmp->n);
+}
