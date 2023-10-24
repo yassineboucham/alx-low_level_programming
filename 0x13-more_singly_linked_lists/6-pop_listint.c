5af41eab43d5bcd16e8915ecfeffc94839697c15
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-24 23:45:20 +0100

    print el

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * pop_listint - pop_listint
+ * @head: listint
+ * Return: tmp->n
+*/
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp = *head;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * get_nodeint_at_index - function that returns the nth node of a listint_t
+ * @head: lintsint
+ * @index: int
+ * Return: tmp
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
