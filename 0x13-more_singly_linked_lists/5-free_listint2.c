3c1d3bc2795ad4ca36b6e8cfdab345ab8785ab02
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-24 23:19:05 +0100

    ex 5 free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,8 @@
 #include "lists.h"
-
+/**
+ *  free_listint2 - deletes the head node of a listint_t linked list
+ * @head: listint_t
+*/
 void free_listint2(listint_t **head)
 {
 	listint_t *curr = *head;
