f27a3e3f776139c3e2ee24c921d413a273859ea6
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-25 00:10:10 +0100

    ex 8

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newnode;
 
 	newnode = (listint_t *)malloc(sizeof(listint_t));
+
 	if (newnode == NULL)
 	return (NULL);
 	newnode->n = n;
