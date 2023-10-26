0997aafe54d61d70754ee351d62996fdaf57a147
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-26 03:33:02 +0100

    read

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: listint
+ * @idx: int
+ * @n: int
+ * Return: new_node
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int in = 0;
