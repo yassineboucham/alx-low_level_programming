c15e603efa317f4e7c5c57bfa8cddcc0f33205bc
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-26 03:30:13 +0100

    read

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,37 @@
 #include "lists.h"
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int in = 0;
-    listint_t *tmp = *head;
-    listint_t *curr;
-    curr = (listint_t *)malloc(sizeof(listint_t));
-    while (in <= idx)
-    {
-        tmp->next = curr;
-        tmp = tmp->next;
-        curr->n = n;
-        curr->next = tmp;
-    }
+	unsigned int in = 0;
+
+	listint_t *curr;
+	listint_t *new_node;
+
+	if (*head == NULL || !head)
+	return (NULL);
+	curr = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		while (in < (idx - 1))
+		{
+			if (curr == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+			curr = curr->next;
+			in++;
+		}
+			new_node->next = curr->next;
+			curr->next = new_node;
+	}
+	return (new_node);
 }
