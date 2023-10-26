b10faef97cab013f8e6383b34b063560fb35390a
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-26 01:22:46 +0100

    ali

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,13 +5,12 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *curr = *head;
+	listint_t *curr;
 
-	while (curr != NULL)
+	while (*head != NULL)
 	{
-		*head = curr;
-		curr = curr->next;
-		free(*head);
+		curr = *head;
+		*head = (*head)->next;
+		free(curr);
 	}
-	*head = NULL;
 }
