b391ca3feb05db94c37aa3ff454041e53c7f4e78
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-26 01:48:41 +0100

    h

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,19 @@
 #include "lists.h"
 /**
- * pop_listint - pop_listint
- * @head: listint
- * Return: tmp->n
+* pop_listint - pop_listint
+* @head: listint
+* Return: tmp->n
 */
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp = *head;
-	*head = tmp->next;
-	return (tmp->n);
+	listint_t *tmp;
+	int i;
+
+	if (!head || !*head)
+	return (0);
+	i = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (i);
 }
