ca30241991998ea10c46181cdf44a1d0277dbd03
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-29 22:39:56 +0000

    all

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * print_binary - unction that prints the binary representation of a number.
- * @n: unsigned long int
+* print_binary - unction that prints the binary representation of a number.
+* @n: unsigned long int
 */
 void print_binary(unsigned long int n)
 {
-	if (n == 0 && n == 1)
-	_putchar(n);
-	_putchar('0' + (n & 1));
 	if (n > 1)
-	print_binary(n >> 1);
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
