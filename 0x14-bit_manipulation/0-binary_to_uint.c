efe9d8a1463a25ab657ccc439f8e5f172626fdba
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-29 22:37:36 +0000

    all

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,27 @@
 #include "main.h"
 /**
- * binary_to_uint - function that converts a binary number to an unsigned int
- * @b: the string
- * Return: number
+* binary_to_uint - function that converts a binary number to an unsigned int
+* @b: the string
+* Return: number
 */
+#include "main.h"
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
 
 	if (!b)
-	return (0);
+		return (0);
+
 	while (*b)
 	{
-		if (*b != 0 && *b != 1)
-		return (0);
+		if (*b != '0' && *b != '1')
+			return (0);
 		i = i << 1;
 		i = i | (*b - '0');
 		b++;
 	}
+
 	return (i);
 }
+
