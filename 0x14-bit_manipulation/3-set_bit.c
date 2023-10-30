3c7f2b897a827266a268b77fbeeceb7f144208ef
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-30 01:28:42 +0000

    all

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * set_bit - a function that sets the value of a bit to 1 at a given index.
- * @n: unsigned long int
- * @index: unsigned int
- * Return: n
+* set_bit - a function that sets the value of a bit to 1 at a given index.
+* @n: unsigned long int
+* @index: unsigned int
+* Return: n
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int nb;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 	nb = 1 << index;
 	*n = *n | nb;
 	return (*n);
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * clear_bit - a function that sets the value of a bit to 0 at a given index.
- * @n: unsigned long int
- * @index: unsigned int
- * Return: n
+* clear_bit - a function that sets the value of a bit to 0 at a given index.
+* @n: unsigned long int
+* @index: unsigned int
+* Return: n
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int nb;
 
-	if (*n == 0)
-	return (0);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 	nb = 1 << index;
 	*n = *n ^ nb;
 	return (*n);
