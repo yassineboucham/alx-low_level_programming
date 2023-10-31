6baaaf4dd14d74e5335155fba54b78e88cc72b57
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-31 02:53:48 +0000

    all

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * read_textfile - that reads a text file and prints
+ * @filename: const char
+ * @letters: size_t
+ * Return: i
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, bytes = 0, i = 0;
+
+	char *string;
+
+	if (filename == NULL)
+	return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	return (0);
+	string = malloc(letters);
+	if (string == NULL)
+	return (0);
+	bytes = read(file, string, letters);
+	i = write(STROUT_FILNO, string, bytes);
+	colse(file);
+	free(string);
+	return (i);
+}
