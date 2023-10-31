27583588e4fd167b1d888c4ae01f616ea30bc47e
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-31 23:15:41 +0000

    ex1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 	if (text_content == NULL)
 	text_content = "";
-	file = open(filename, O_CREAT | O_WRONLY, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 	return (-1);
 	bite = write(file, text_content, sstrlen(text_content));
