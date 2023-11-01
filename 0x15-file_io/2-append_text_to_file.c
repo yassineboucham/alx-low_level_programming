f530fe0d698d509bb59143e2b02691b732a9a133
root <root@DESKTOP-GSM3MPE.localdomain>
2023-11-01 00:46:30 +0000

    all

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include "strlen.c"
 /**
- * append_text_to_file -  function that appends text at the end of a file.
- * @filename: const char
- * @text_content: char
- * Return: 1
+* append_text_to_file -  function that appends text at the end of a file.
+* @filename: const char
+* @text_content: char
+* Return: 1
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int byte, file;
 
+	if (!text_content)
+	text_content = "";
 	if (!filename)
 	return (-1);
 	file = open(filename, O_APPEND | O_WRONLY);
