98cdfba4fe307d6c40eba7c08b289ecdd02a965d
yassineboucham <yassineboucham11@gamil.com>
2023-12-12 01:17:08 +0000

    hadchi zawin

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,26 +1,22 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: dlistint_t
- * @idx: unsigned int
- * @n: int
- * Return: node
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: dlistint_t
+* @idx: unsigned int
+* @n: int
+* Return: node
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-
-	dlistint_t *crt = *h;
-	dlistint_t *node;
+	dlistint_t *crt = *h, *node;
 
 	if (idx == 0)
 	{
 		node = malloc(sizeof(dlistint_t));
 		if (!node)
 			return (NULL);
-		node->prev = NULL;
-		node->next = crt;
-		node->n = n;
+		node->prev = NULL, node->next = crt, node->n = n;
 		if (crt)
 			crt->prev = node;
 		*h = node;
@@ -33,15 +29,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			node = malloc(sizeof(dlistint_t));
 			if (!node)
 				return (NULL);
-			node->prev = crt->prev;
-			node->n = n;
-			node->next = crt;
+			node->prev = crt->prev, node->n = n, node->next = crt;
 			if (crt->prev)
 				crt->prev->next = node;
 			crt->prev = node;
 			return (node);
+			crt = crt->next;
 		}
-		crt = crt->next;
+		if (i == idx)
+		{
+			node = malloc(sizeof(dlistint_t));
+			if (!node)
+				return (NULL);
+			node->next = NULL, node->prev = crt;
+			if (crt)
+				crt->next = node;
+			return (node);
+		}
 	}
 	return (NULL);
 }
