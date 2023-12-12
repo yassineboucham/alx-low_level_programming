f57fcaf8d0d84350d562debf03c2248ef3c410ff
yassineboucham <yassineboucham11@gamil.com>
2023-12-12 01:26:05 +0000

    hadchi zawin

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,65 +1,51 @@
 #include "lists.h"
-
+/**
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: dlistint_t
+* @idx: unsigned int
+* @n: int
+* Return: node
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
+	dlistint_t *crt = *h, *node;
 
-    dlistint_t *crt = *h;
-    dlistint_t *node;
-
-    if (idx == 0)
-    {
-        node = malloc(sizeof(dlistint_t));
-        if (!node)
-            return (NULL);
-
-        node->n = n;
-        node->prev = NULL;
-        node->next = crt;
-
-        if (crt)
-            crt->prev = node;
-
-        *h = node;
-        return (node);
-    }
-
-    while (crt)
-    {
-        if (i++ == idx)
-        {
-            node = malloc(sizeof(dlistint_t));
-            if (!node)
-                return (NULL);
-
-            node->n = n;
-            node->prev = crt->prev;
-            node->next = crt;
-
-            if (crt->prev)
-                crt->prev->next = node;
-
-            crt->prev = node;
-            return (node);
-        }
-        crt = crt->next;
-    }
-
-    if (i == idx)
-    {
-        node = malloc(sizeof(dlistint_t));
-        if (!node)
-            return (NULL);
-
-        node->n = n;
-        node->prev = crt;
-        node->next = NULL;
-
-        if (crt)
-            crt->next = node;
-
-        return (node);
-    }
-
-    return (NULL);
+	if (idx == 0)
+	{
+		node = malloc(sizeof(dlistint_t));
+		if (!node)
+			return (NULL);
+		node->prev = NULL, node->next = crt, node->n = n;
+		if (crt)
+			crt->prev = node;
+		*h = node;
+		return (node);
+	}
+	while (crt)
+	{
+		if (i++ == idx)
+		{
+			node = malloc(sizeof(dlistint_t));
+			if (!node)
+				return (NULL);
+			node->prev = crt->prev, node->n = n, node->next = crt;
+			if (crt->prev)
+				crt->prev->next = node;
+			crt->prev = node;
+			return (node);
+		}
+		crt = crt->next;
+	}
+		if (i == idx)
+		{
+			node = malloc(sizeof(dlistint_t));
+			if (!node)
+				return (NULL);
+			node->next = NULL, node->prev = crt, node->n = n;
+			if (crt)
+				crt->next = node;
+			return (node);
+		}
+	return (NULL);
 }
