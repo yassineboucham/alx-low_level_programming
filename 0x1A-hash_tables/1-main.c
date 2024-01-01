506f190d75f71fe6a4d1bfde892d35ce0e6279ea
yassineboucham <yassineboucham11@gamil.com>
2024-01-01 11:40:52 +0000

    4

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	char *s;
 
 	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%u\n", hash_djb2((unsigned char *)s));
 	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%u\n", hash_djb2((unsigned char *)s));
 	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%u\n", hash_djb2((unsigned char *)s));
 	return (EXIT_SUCCESS);
 }
