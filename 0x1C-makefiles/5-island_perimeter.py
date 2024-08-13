d9c4a53aa3ac216f809820937f158bbeee29a18f
yassineboucham <yassineboucham2002@gmail.com>
2024-08-13 22:43:16 +0100

    edit style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,9 +14,9 @@ def island_perimeter(grid):
     for i in range(h):
         for j in range(w):
             if grid[i][j] == 1:
-                s+=1
+                s += 1
                 if (j > 0 and grid[i][j - 1] == 1):
-                    edges+=1
+                    edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
-                    edges+=1
+                    edges += 1
     return ((s * 4) - (edges * 2))
